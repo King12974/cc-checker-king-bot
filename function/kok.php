b2a587b5e396c3c811213a9142d8f94c79157e05
King12974 <farukking518@gmail.com>
2024-12-14 22:30:06 +0530

    Update kok.php

--- function/kok.php
+++ function/kok.php
@@ -5,7 +5,7 @@ $currentDate = date('Y-m-d');
     $expiryDate = "0";
 
 
-$videoURLStart = "https://t.me/videobyblackheadsop/2";
+$videoURLStart = "https://t.me/bgmiidsellardm/39";
 
 $paidUsers = file('Database/paid.txt', FILE_IGNORE_NEW_LINES);
 $freeUsers = file('Database/free.txt', FILE_IGNORE_NEW_LINES);
